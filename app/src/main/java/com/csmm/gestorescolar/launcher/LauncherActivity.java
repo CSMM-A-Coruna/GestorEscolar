749c2ea533276547059770617d8602bb1b53eea2
SCMM <scmm@MacBook-Air-de-Javi.local>
2022-03-22 15:37:26 +0100

    Creado el proyecto base, añadidas LoginActivity y SplashScreen

--- app/src/main/java/com/csmm/gestorescolar/launcher/LauncherActivity.java
+++ app/src/main/java/com/csmm/gestorescolar/launcher/LauncherActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.csmm.gestorescolar.R;
 import com.csmm.gestorescolar.screens.auth.LoginActivity;
 
 public class LauncherActivity extends AppCompatActivity {
@@ -13,9 +12,7 @@ public class LauncherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Iniciamos la actividad Login
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, SplashActivity.class);
         startActivity(intent);
     }
 }
