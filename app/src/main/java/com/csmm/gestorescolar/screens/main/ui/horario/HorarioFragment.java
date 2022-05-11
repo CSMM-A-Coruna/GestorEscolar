15f0e4619322ebd12352d0e8be496f13efdca69b
javier.miralles <javimr2001@gmail.com>
2022-05-11 08:38:25 +0200

    ...

--- app/src/main/java/com/csmm/gestorescolar/screens/main/ui/horario/HorarioFragment.java
+++ app/src/main/java/com/csmm/gestorescolar/screens/main/ui/horario/HorarioFragment.java
@@ -1,32 +1,78 @@
 package com.csmm.gestorescolar.screens.main.ui.horario;
 
+import android.graphics.RectF;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+import com.csmm.gestorescolar.R;
 import com.csmm.gestorescolar.databinding.HorarioFragmentBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 
 public class HorarioFragment extends Fragment {
 
     private HorarioFragmentBinding binding;
+    private WeekView mWeekView;
+    private MonthLoader.MonthChangeListener mMonthChangeListener;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HorarioViewModel horarioViewModel =
-                new ViewModelProvider(this).get(HorarioViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         binding = HorarioFragmentBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHorario;
-        horarioViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Get a reference for the week view in the layout.
+        mWeekView = (WeekView) root.findViewById(R.id.weekView);
+
+        MonthLoader.MonthChangeListener mMonthChangeListener = new MonthLoader.MonthChangeListener() {
+            @Override
+            public List<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+                // Populate the week view with some events.
+                List<WeekViewEvent> events = getEvents(newYear, newMonth);
+                return events;
+            }
+        };
+
+        // Set an action when any event is clicked.
+        mWeekView.setOnEventClickListener(new WeekView.EventClickListener() {
+            @Override
+            public void onEventClick(WeekViewEvent event, RectF eventRect) {
+
+            }
+        });
+
+        // The week view has infinite scrolling horizontally. We have to provide the events of a
+        // month every time the month changes on the week view.
+        mWeekView.setMonthChangeListener(mMonthChangeListener);
+
+        // Set long press listener for events.
+        mWeekView.setEventLongPressListener(new WeekView.EventLongPressListener() {
+            @Override
+            public void onEventLongPress(WeekViewEvent event, RectF eventRect) {
+
+            }
+        });
+
         return root;
+    }
+
+    private List<WeekViewEvent> getEvents(int newYear, int newMonth) {
+
+        List<WeekViewEvent> lista = new ArrayList<>();
+        lista.add(weekViewEvent);
+        return lista;
     }
 
     @Override
