5bc5690c60eb67e4fb1375054b291080c8a5ea8b
FlagshipDev <javimr2001@gmail.com>
2022-05-18 16:05:55 +0200

    Creado el cálculo de tipo de documento (terminación) y el formateo de la hora

--- app/src/main/java/com/csmm/gestorescolar/client/dtos/DocumentoDTO.java
+++ app/src/main/java/com/csmm/gestorescolar/client/dtos/DocumentoDTO.java
@@ -10,14 +10,17 @@ public class DocumentoDTO {
     private String enlace;
     private String categoria;
     private String fecha;
+    private String tipoDocumento;
 
     public DocumentoDTO(JSONObject json) {
         try {
-            idDocumento = json.getInt("id_documento");
-            documento = json.getString("documento");
-            enlace =  json.getString("enlace");
-            categoria = json.getString("categoria");
-            fecha = json.getString("fecha");
+            this.idDocumento = json.getInt("id_documento");
+            this.documento = json.getString("documento");
+            this.enlace =  json.getString("enlace");
+            this.categoria = json.getString("categoria");
+            String[] splited = json.getString("fecha").split("T");
+            this.fecha = splited[0];
+            this.tipoDocumento = calcularTipoDocumento(json.getString("enlace"));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -41,5 +44,21 @@ public class DocumentoDTO {
 
     public String getFecha() {
         return fecha;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    private String calcularTipoDocumento(String enlace) {
+        try {
+            String[] splited = enlace.split("/");
+            String documento = splited[splited.length-1];
+            String[] splited2 = documento.split("\\.");
+            return splited2[splited2.length-1];
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "default";
+        }
     }
 }
