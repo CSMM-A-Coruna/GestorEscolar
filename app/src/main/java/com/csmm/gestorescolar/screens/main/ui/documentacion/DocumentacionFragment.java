64dfd82bfbb6cb0d1c32cc3f33c776faf23c0dcc
FlagshipDev <javimr2001@gmail.com>
2022-05-18 11:38:23 +0200

    Corregido un error con los forEach (BuildSDK)

--- app/src/main/java/com/csmm/gestorescolar/screens/main/ui/documentacion/DocumentacionFragment.java
+++ app/src/main/java/com/csmm/gestorescolar/screens/main/ui/documentacion/DocumentacionFragment.java
@@ -4,11 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.csmm.gestorescolar.databinding.DocumentacionFragmentBinding;
 
@@ -16,16 +14,11 @@ public class DocumentacionFragment extends Fragment {
 
     private DocumentacionFragmentBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        DocumentacionViewModel documentacionViewModel =
-                new ViewModelProvider(this).get(DocumentacionViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         binding = DocumentacionFragmentBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDocumentacion;
-        documentacionViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
